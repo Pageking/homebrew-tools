8c13bd55ffd9a2c3455cede53ff1c4e33502898d
Pageking <info@pageking.nl>
2025-07-24 12:00:42 +0200

    initial commit

---
+++ Formula/devinit.rb
@@ -0,0 +1,22 @@
+class Devinit < Formula
+  desc "Pageking CLI for initializing projects"
+  homepage "https://github.com/Pageking/devinit"
+  url "https://github.com/Pageking/devinit.git"
+  version "1.0.0"
+  license "MIT"
+  head "https://github.com/Pageking/devinit.git"
+
+  depends_on "jq"
+  depends_on "gh"
+  depends_on "git"
+
+  def install
+    bin.install "devinit"
+    libexec.install "lib"
+    prefix.install "config"
+  end
+
+  test do
+    system "#{bin}/devinit"
+  end
+end
