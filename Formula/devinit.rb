6cba422c625483271b8125d654c1f537f6b070ca
Pageking <info@pageking.nl>
2025-08-15 10:33:23 +0200

    v1.1.2

--- Formula/devinit.rb
+++ Formula/devinit.rb
@@ -1,9 +1,9 @@
 class Devinit < Formula
   desc "Pageking CLI for initializing projects"
   homepage "https://github.com/Pageking/devinit"
-  url "https://github.com/Pageking/devinit/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "d96321fc255d52041194464ecad9ff50301c6056892ae71abc91179ebef0646b"
-  version "1.1.1"
+  url "https://github.com/Pageking/devinit/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "8150d8314104722ded607d5708a25ab83781fa0b031848528119beb857ba52a6"
+  version "1.1.2"
   license "MIT"
   head "https://github.com/Pageking/devinit.git"
 
