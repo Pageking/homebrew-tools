837ca6e9e4807f9a214a11018be403b333c0d87b
Pageking <info@pageking.nl>
2025-10-24 16:50:07 +0200

    v1.0.34-beta

--- Formula/pixel.rb
+++ Formula/pixel.rb
@@ -7,6 +7,12 @@ class Pixel < Formula
   license ""
   head "https://github.com/Pageking/pixel.git"
 
+  devel do
+    url "https://github.com/Pageking/pixel/archive/refs/tags/v1.0.34-beta.tar.gz"
+    sha256 "f1a53f5ba7cc123150d18472e6cc2d4f739a3c73253eac312da82590af6ee1bd"
+    version "1.0.34-beta"
+  end
+
   depends_on "jq"
   depends_on "gh"
   depends_on "git"
