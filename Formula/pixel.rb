34cc3ad05d9271a057f8f7a1351ce5e02e45efa8
Pageking <info@pageking.nl>
2026-04-22 11:08:10 +0200

    v1.4.10

--- Formula/pixel.rb
+++ Formula/pixel.rb
@@ -1,9 +1,9 @@
 class Pixel < Formula
   desc "Pageking CLI for initializing projects"
   homepage "https://github.com/Pageking/pixel"
-  url "https://github.com/Pageking/pixel/archive/refs/tags/v1.4.9.tar.gz"
-  sha256 "e5ac6d416a425a4ca301f3f23d14d19bebfba66602171475cca571bf4a0cebb1"
-  version "1.4.9"
+  url "https://github.com/Pageking/pixel/archive/refs/tags/v1.4.10.tar.gz"
+  sha256 "661cb57b67631e23cab0c756743dd99304e460b737d2fdbb0acadfc09fecb16c"
+  version "1.4.10"
   license ""
   head "https://github.com/Pageking/pixel.git"
 
